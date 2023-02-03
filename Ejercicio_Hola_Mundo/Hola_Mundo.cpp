2848688e9fec9203355c1cdc89cd6cbfba30071e
Giovanni Cifuentes <giovannicifuentescss20@gmail.com>
2023-02-03 17:03:11 -0600

    Ejercicio Hola Mundo 2.0

---
+++ Ejercicio_Hola_Mundo/Hola_Mundo.cpp
@@ -0,0 +1,26 @@
+//nombre: Giovanni Cifuentes
+//carné: 9959-22-1822
+
+
+//definicion de bliblioteca estandar
+
+#include <iostream>
+// iostream permite el ingreso y salida de archivos para la pantalla y
+//desde la pantalla, se defien en librerias standar regulares
+
+using namespace std;
+//definiendo el nombre usado "estadar"
+//todos los comandos van finalizando con ;
+
+// int (entero)
+// esta es la funcion principal que siempre debe de existir ""main"
+int main()
+{
+    cout << "Hola Mundo" << endl;
+    //imprime hola mundo, si hay un string o cadena se debe de usar ""
+    // << son redicionadores de flujo y el otro << es para cambiar de linea
+
+    return 0;
+    //debe de retornar, como es funcion int (entero) es 0
+}
+//todo bloque inicia y termina con {}//
